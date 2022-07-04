538654874547cf79c9ca382f984343ff75de8250
Yanhong Zhao <yanhong.zhao@ef.com>
2022-07-05 00:04:28 +0100

    add date checking

--- robyn_data_validation/data_review.py
+++ robyn_data_validation/data_review.py
@@ -1,3 +1,6 @@
+import warnings
+from typing import Union
+
 import pandas as pd
 
 
@@ -14,9 +17,16 @@ class DataReviewer:
         self,
         file_path: str = None,
         data_frame: pd.DataFrame = pd.DataFrame(),
+        date_column_name: str = "DATE",
+        date_format: Union[str, None] = None,
         date_frequency: str = "weekly",
     ) -> None:
         self.data = self._read_input_data_source(file_path, data_frame)
+        # convert the data's date column upfront
+        self.date_column_name = date_column_name
+        self.data[date_column_name] = pd.to_datetime(
+            self.data[date_column_name], format=date_format
+        )
         self.date_frequency = date_frequency
         self._check_date_frequency()
 
@@ -34,8 +44,18 @@ class DataReviewer:
         else:
             return pd.read_csv(file_path)
 
-    def _check_date_frequency(self):
+    def _check_date_frequency(self, threshold: float = 0.8):
         assert self.date_frequency in [
             "weekly",
             "daily",
         ], "only daily or weekly frequency is currently supported"
+        temp_df = self.data.set_index(self.date_column_name)
+        if self.date_frequency == "daily":
+            counts = temp_df[temp_df.columns[0]].resample("1D").count()
+        else:
+            counts = temp_df[temp_df.columns[0]].resample("1W").count()
+        if (counts == 1).mean() < threshold:
+            warnings.warn(
+                "Please check if the date frequency is rightly selected or check if there are substantial time gaps in the dataset"
+            )
+        return None
